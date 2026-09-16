cc039da849e96b51f17b3b9a7ea417d028114204
Mani <muniramm890@gmail.com>
2026-09-16 14:33:40 +0530

    Add notifications route to server

--- src/server.js
+++ src/server.js
@@ -96,6 +96,7 @@ app.use(`${API}/timetable`,    require('./routes/timetables'));
 app.use(`${API}/quick-tests`,   require('./routes/quickTests'));
 app.use(`${API}/exams`,         require('./routes/exams'));
 app.use(`${API}/notices`,       require('./routes/notices'));
+app.use(`${API}/notifications`, require('./routes/notifications'));
 app.use(`${API}/comm`, require('./routes/commHub'));
 app.use(`${API}/homework`,      require('./routes/homework'));
 app.use('/api/attendance',authenticate,   require('./routes/attendance'));
