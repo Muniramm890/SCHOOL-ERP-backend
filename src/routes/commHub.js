f8c497bb4437d0d4fdaebd8dbfb808d327345562
Mani <muniramm890@gmail.com>
2026-09-16 14:37:05 +0530

    Add route to get viewers for a message

--- src/routes/commHub.js
+++ src/routes/commHub.js
@@ -8,6 +8,7 @@ router.use(authenticate);
 router.post('/preview', ctrl.previewTargets);
 router.get('/messages', ctrl.listMessages);
 router.get('/messages/:id', ctrl.getMessage);
+router.get('/messages/:id/viewers', ctrl.getViewers);
 router.post('/messages', authorize('admin', 'principal', 'teacher'), upload.uploadCommAttachment.array('attachments', 3), ctrl.createAndSend);
 
 module.exports = router;
