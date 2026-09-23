66e587428ba390cfe6d5ee152d16637baa14bcba
Mani <muniramm890@gmail.com>
2026-09-23 17:11:37 +0530

    Add Cashfree payment routes to payments.js

--- src/routes/payments.js
+++ src/routes/payments.js
@@ -8,5 +8,7 @@ router.use(authenticate, requireSchool);
 
 router.post('/razorpay/create-order', paymentsController.createOrder);
 router.post('/razorpay/verify', paymentsController.verifyAndRecord);
+router.post('/cashfree/create-order', paymentsController.createCashfreeOrder);
+router.post('/cashfree/verify', paymentsController.verifyCashfreePayment);
 
 module.exports = router;
