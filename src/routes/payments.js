9e5b405d0505edbb0387f93e616e91ab6a2a2636
Mani <muniramm890@gmail.com>
2026-09-26 15:18:56 +0530

    Add middleware for Cashfree order creation
    Added middleware to set appOrigin for Cashfree order creation.

--- src/routes/payments.js
+++ src/routes/payments.js
@@ -8,7 +8,10 @@ router.use(authenticate, requireSchool);
 
 router.post('/razorpay/create-order', paymentsController.createOrder);
 router.post('/razorpay/verify', paymentsController.verifyAndRecord);
-router.post('/cashfree/create-order', paymentsController.createCashfreeOrder);
+router.post('/cashfree/create-order', (req, res, next) => {
+  req.appOrigin = process.env.ADMIN_FRONTEND_URL;
+  next();
+}, paymentsController.createCashfreeOrder);
 router.post('/cashfree/verify', paymentsController.verifyCashfreePayment);
 
 module.exports = router;
